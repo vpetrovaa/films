f6b33b90e28975e9d62876b4dfbf375055273f42
vpetrovaa <vpetrova.laba@solvd.com>
2023-03-10 13:10:11 +0300

    Db changes were added

--- src/main/resources/db.changelog/changeset/001-create-tables.sql
+++ src/main/resources/db.changelog/changeset/001-create-tables.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset vpetrovaa:create_users_tables
+--changeset vpetrovaa:create_films_tables
 
 create schema if not exists cinema_schema;
 set schema 'cinema_schema';
@@ -12,5 +12,6 @@ create table if not exists films
 	year int not null,
 	genre varchar(45) not null,
 	showing_date timestamp not null,
+	price real not null,
 	primary key (id)
 );
